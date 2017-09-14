cadce407c6a4ad2911d645fa242dec994886e7ac
usuario <usuario@DESKTOP-8LIEST6>
2017-09-14 19:10:57 -0300

    Corrigindo erro do menor valor. 14-09-2017 | 19:10

--- Lab01/src/lab01/Lab01.java
+++ Lab01/src/lab01/Lab01.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Universidade Estadual da Paraíba
+*  Atividade de Laboratório 01 - Prof.: Daniel Gondim
+*  Alunos: Higor Pereira / Lanmark Rafael / Danilo de Souza
  */
 package lab01;
 
@@ -10,27 +9,33 @@ import java.util.Scanner;
 public class Lab01 {
 
     public static void main(String[] args) {
+        
         float num, maior, menor, numeroCompare;
         maior = 0;
         menor = 0;
         int count = 1;
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite quanto numeros vão ser comparados: ");
+        System.out.println("Informe a quantidade de números a serem comparados: ");
         numeroCompare = entrada.nextFloat();
 
         while (count <= numeroCompare) {
             System.out.print("Numero " + count + " : ");
             num = entrada.nextFloat();
+
+            if (maior == 0 && menor == 0) {
+                maior = num;
+                menor = num;
+            }
             if (num > maior) {
                 maior = num;
             }
-            if (num < maior) {
+            if (num < menor) {
                 menor = num;
             }
             count++;
         }
-        System.out.println("O maior numero digitado foi: " + maior);
-        System.out.println("O menor numero digitado foi: " + menor);
+        System.out.println("O maior número informado foi: " + maior);
+        System.out.println("O menor número informado foi: " + menor);
     }
 }
